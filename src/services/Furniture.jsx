b0cc1fddeccf49a86223c0e61d2ba9be63f8d01c
Mohamed Mosa <mohamed24halim@gmail.com>
2024-10-07 04:00:52 +0300

    Update Furniture.jsx

--- src/services/Furniture.jsx
+++ src/services/Furniture.jsx
@@ -68,7 +68,7 @@ const Furniture = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-6 h-6">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1" d="M9 5l7 7-7 7" />
                         </svg>
-                        <Link to="/jewelery" className="sm:text-2xl text-xl text-blue-600">Furniture</Link>    
+                        <Link to="/jewelery" className="sm:text-2xl  text-xl text-blue-600">Furniture</Link>    
                     </div>
                 </div>
 
